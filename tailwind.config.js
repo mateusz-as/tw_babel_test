c93ef48e9c807152ecaf7a9dbe1ade7d2aaf3a85
Mateusz Ramski <mateusz.ramski@amsterdamstandard.com>
2021-06-21 14:58:02 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    theme: {
+        colors: {
+            primary: colors.black
+        }
+    }
+}
